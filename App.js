9bad8ac4ecb4b2e250226b425db1be6e9c376ecd
lee Jeong Eun <wjd7dms7@naver.com>
2022-10-13 18:10:13 +0900

    #2.8

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
 import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
-console.log(SCREEN_WIDTH);
+const API_KEY = "071ddc563aa4f0fd47b4922ff0086e2e"
+// 이번강의에서만 이렇게 사용, 이건 안전한 방법이 아니다
 
 export default function App() {
-  const [location, setLocation] = useState(null);
+  const [city, setCity] = useState("Loading...")
+  const [minCity, setMinCity] = useState("Loading...")
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async() => {
-    const permission = await Location.requestForegroundPermissionsAsync()
-  }
-  // useEffect(() => {
-  //   ask();
-  // }, [])
-  
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    } // 유저가 위치정보 추적 권한을 거절했을 경우
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].region)
+    setMinCity(location[0].district)
+    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`)
+    const json = await response.json();
+    console.log(json)
+  };
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
+        <Text style={styles.McityName}>{minCity}</Text>
       </View>
       <ScrollView
         pagingEnabled
@@ -60,7 +79,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   cityName: {
-    fontSize: 68,
+    fontSize: 46,
+    fontWeight: "500",
+  },
+  McityName: {
+    fontSize: 65,
     fontWeight: "500",
   },
   weather: {},
